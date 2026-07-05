2f132e28ccf569d74280d90747831b92824224c8
flamonteiro <fla.mont2019@gmail.com>
2026-07-05 17:25:00 -0300

    feature: código inicial do index e assets relacionados

--- views/index.php
+++ views/index.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Distribuidora de Bebidas</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php require_once "includes/cabecalho.inc.php" ?>
+
+<!-- CONTEUDO -->
+<h1 class="text-center">Bem-vindos à nossa distribuidora!</h1>
+
+<section class="my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto fs-5" align="justify">
+        <img src="imagens/header.jpg" alt="" class="img-fluid"><br><br>
+        <p>Que tal garantir as bebidas para o seu churrasco, festa ou resenha sem precisar sair de casa? Então confira a nossa loja virtual. <strong>As mesmas marcas, variedade e qualidade que você já encontra na nossa distribuidora física!</strong> Agora também online para abastecer os seus melhores momentos!</p>
+        <p>Nossa Distribuidora oferece uma experiência única para você! Aqui você escolhe se quer comprar na nossa distribuidora física ou virtual, se quer receber suas bebidas trincando de geladas por delivery ou retirar direto com a gente, e se quer pagar com dinheiro, cartão de débito ou crédito, ou via PIX! Só na Loja Des Web você compra do seu jeito!</p>
+        <p>Confira nosso estoque de <strong>cervejas, destilados, refrigerantes e muito mais</strong>, e faça suas compras aproveitando nossos Cupons de Desconto, melhores ofertas e promoções imperdíveis!</p>   
+      </div> 
+    </div>
+  </div>
+</section>
+
+<!-- Rodape -->
+
+<?php require_once "includes/rodape.inc.php" ?>
